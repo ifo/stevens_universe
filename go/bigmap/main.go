88e51a7f58c4a3e2e7a4ad99bcdcea9ebac07ac4
Steve McCarthy <steve@redlua.com>
2016-05-15 21:24:05 -0400

    Use empty structs to save space

--- go/bigmap/main.go
+++ go/bigmap/main.go
@@ -1,18 +1,17 @@
 package main
 
-type Node struct {
-	Value string
-}
-
 func main() {
-	AllPossibleStrings("abcd", 12)
+	AllPossibleStrings("abcd", 11)
+	// wait forever to look at memory usage
+	for {
+	}
 }
 
 // AllPossibleStrings will generate len(alphabet) ^ length number of strings
-func AllPossibleStrings(alphabet string, length int) map[string]*Node {
-	out := map[string]*Node{}
+func AllPossibleStrings(alphabet string, length int) map[string]struct{} {
+	out := map[string]struct{}{}
 	for _, r := range alphabet {
-		out[string(r)] = &Node{}
+		out[string(r)] = struct{}{}
 	}
 	for i := 0; i < length-1; i++ {
 		out = AddAnotherCharacter(out, alphabet)
@@ -20,11 +19,11 @@ func AllPossibleStrings(alphabet string, length int) map[string]*Node {
 	return out
 }
 
-func AddAnotherCharacter(c map[string]*Node, alphabet string) map[string]*Node {
-	out := map[string]*Node{}
+func AddAnotherCharacter(c map[string]struct{}, alphabet string) map[string]struct{} {
+	out := map[string]struct{}{}
 	for str := range c {
 		for _, r := range alphabet {
-			out[str+string(r)] = &Node{}
+			out[str+string(r)] = struct{}{}
 		}
 	}
 	return out
