1ef8d33dc9d912a115ee3ff9abdc96fc7b5b9096
Steve McCarthy <steve@redlua.com>
2019-01-17 23:24:05 -0500

    rust book minigrep - move run into its own function

--- rust/rustbook/minigrep/src/main.rs
+++ rust/rustbook/minigrep/src/main.rs
@@ -13,10 +13,14 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
+    run(config);
+}
+
+fn run(config: Config) {
+   let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    println!("With text:\n{}", contents); 
 }
 
 struct Config {
